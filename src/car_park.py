9f81702b3acad8162a5ea45ab2d5e62dc4831946
vanishlabs <l.hutchings_richards@hotmail.com>
2025-12-10 00:41:36 +0800

    feature(carpark,display,sensor): base implementation
    carpark:
    - fixed communication directions so sensors update carpark and carpark updates displays.
    - implemented available_spaces property, register method to register displays, update_temp method to update ambient_temp attribute.
    - implemented update displays method to iterate over our registered displays and update the message.
    
    display:
    - changed is_on attribute to is_active for consistency sake
    - implemented update method which gets called by our carpark to update the message.
    
    sensor:
    - implemented exit and entry sensor which inherits from a parking sensor base class (eg. exit -> parking -> base sensor), these update our carpark plate list when a vehicle is detected.
    - implemented temp sensor class which reads ambient temp and updates car park attribute

--- src/car_park.py
+++ src/car_park.py
@@ -1,5 +1,4 @@
-from sensor import Sensor
-from display import Display
+from .display import Display
 
 
 class CarPark:
@@ -7,42 +6,50 @@ class CarPark:
         self,
         location: str,
         capacity: int,
-        sensors: list[Sensor] | None = None,
         displays: list[Display] | None = None,
         plates: list[str] | None = None,
     ) -> None:
         self.capacity = capacity
         self.location = location
         self.displays = displays or []
-        self.sensors = sensors or []
         self.plates = plates or []
+        self.ambient_temp = 0
 
     def __str__(self) -> str:
         return f"Car park is located at {self.location} with capacity {self.capacity}"
+
+    @property
+    def available_spaces(self) -> int:
+        return max(0, self.capacity - len(self.plates))
+
+    def register(self, display: Display) -> None:
+        self.displays.append(display)
+
+    # was going to use a property instead of method here but thought
+    #  i'd just stick with the way we're already updating our carpark
+    def update_temp(self, temp: float) -> None:
+        self.ambient_temp = temp
+        self.update_displays()
 
     def add_car(self, plate: str) -> None:
         if plate in self.plates:
             raise ValueError("This plate is already registered in the car park.")
 
-        if len(self.plates) >= self.capacity:
-            raise ValueError("Car park is full.")
-
         self.plates.append(plate)
+        self.update_displays()
 
     def remove_car(self, plate: str) -> None:
         if plate not in self.plates:
             raise ValueError("This plate is not registered in the car park.")
 
         self.plates.remove(plate)
+        self.update_displays()
 
     def update_displays(self) -> None:
-        spaces = self.capacity - len(self.plates)
+        data: dict[str, int | float] = {
+            "available_spaces": self.available_spaces,
+            "temperature": self.ambient_temp,
+        }
+
         for display in self.displays:
-            display.message = f"Available spaces: {spaces}\nAmbient Temp: "
-
-    def register(self, component: Sensor | Display) -> None:
-        if isinstance(component, Display):
-            self.displays.append(component)
-
-        if isinstance(component, Sensor):
-            self.sensors.append(component)
+            display.update(data)
--- src/display.py
+++ src/display.py
@@ -1,8 +1,13 @@
 class Display:
-    def __init__(self, id: str, message: str ="", is_on: bool =False) -> None:
+    def __init__(self, id: str, message: str = "", is_active: bool = False) -> None:
         self.id = id
         self.message = message
-        self.is_on = is_on
+        self.is_active = is_active
 
     def __str__(self) -> str:
         return f"Display {self.id}: {self.message}"
+
+    def update(self, data: dict[str, int | float]) -> None:
+        self.message = ""
+        for key, value in data.items():
+            self.message += f"{key.replace('_', ' ').capitalize()}: {value}\n"
