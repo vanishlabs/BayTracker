c912b7eea129b8eacb2011e842fa2e745df2f4d1
lachy <86326882+vanishlabs@users.noreply.github.com>
2025-12-10 14:06:58 +0800

    docs: project documentation
    Updated readme.md to include project explanation and examples. 
    Added example code in main.py.

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# aea
+# BayTracker
+Lachlan Hutchings-Richards
+20035488
+
+#### Example Usage
+Uncontrolled CarPark (don't register sensors/displays etc.)
+```python
+    car_park = CarPark("123 Example Location", 100)
+    car_park_from_config = CarPark.from_config(Path("test-config.json")) # from config
+```
+Controlled CarPark
+```python
+    car_park = CarPark("123 Example Location", 100)
+
+    # create our display and register it with our carpark
+    display = Display("display1", "Example Message", True)
+    car_park.register(display)
+
+    # create our entry and exit sensors and inject our carpark
+    entry_sensor = EntrySensor("entry_sensor1", True, car_park)
+    exit_sensor = ExitSensor("exit_sensor1", True, car_park)
+
+    # create our temp sensor (ambient temp returns 0 in uncontrolled lots)
+    temp_sensor = TempSensor("temp_sensor1", True, car_park)
+```
+
+#### Example Configuration File
+```json
+{ 
+    "location": "123 Example location",
+    "capacity": 100,
+    "plates": ["FAKE-001", "FAKE-002"]
+}
+```
+
+#### Test Coverage
+- CarPark
+  - Initialised correctly 
+  - Initialised correctly from config 
+  - Write config to disk 
+  - Add car
+  - Remove car
+  - Overfill carpark
+  - Remove car that doesn't exist
+- Display
+  - Initialised correctly
+  - Display correct message
+  - Update on add car
+- Sensors
+  - Temp and Parking sensors initialised correctly
+  - Temp sensor update carpark
+  - Parking sensor update carpark
+  - Parking sensor detect vehicle  
--- src/sensor.py
+++ src/sensor.py
@@ -8,20 +8,45 @@ if TYPE_CHECKING:
 
 
 class Sensor(ABC):
+    """
+    Abstract base class for all sensors in our carpark manager.
+    """
+
     def __init__(self, id: str, is_active: bool, car_park: CarPark) -> None:
+        """
+        Super constructor for our sensors.
+
+        :param id: The unique ID for the sensor.
+        :type id: str
+        :param is_active: If the sensor should be operational or not.
+        :type is_active: bool
+        :param car_park: The carpark to attach the sensor to.
+        :type car_park: CarPark
+        """
         self.id = id
         self.is_active = is_active
         self.car_park = car_park
 
     @abstractmethod
     def update_car_park(self, data: str | float) -> None:
+        """
+        Abstract method for updating our carpark.
+
+        :param data: The data to send to the carpark.
+        :type data: str | float
+        """
         pass
 
 
 class TempSensor(Sensor):
+    """
+    Concrete implementation of our base Sensor class to 'measure' ambient temp
+    """
+
     def __init__(self, id: str, is_active: bool, car_park: CarPark) -> None:
         super().__init__(id, is_active, car_park)
 
+    # detect fake temp change (call in tests)
     def detect_temp(self) -> None:
         if not self.is_active:
             return
@@ -29,9 +54,11 @@ class TempSensor(Sensor):
         temp = self.check_temp()
         self.update_car_park(temp)
 
+    # return mock temp
     def check_temp(self) -> float:
         return random.uniform(18.0, 38.0)
 
+    # update carpark implementation
     def update_car_park(self, data: str | float) -> None:
         if isinstance(data, str):
             raise TypeError("data cannot be str here.")
@@ -41,9 +68,14 @@ class TempSensor(Sensor):
 
 
 class ParkingSensor(Sensor):
+    """
+    Base class for sensors that detect vehicles. Inherits from our sensor class.
+    """
+
     def __init__(self, id: str, is_active: bool, car_park: CarPark) -> None:
         super().__init__(id, is_active, car_park)
 
+    # detect fake vehicle (call in tests)
     def detect_vehicle(self) -> None:
         if not self.is_active:
             return
@@ -51,14 +83,20 @@ class ParkingSensor(Sensor):
         plate = self._scan_plate()
         self.update_car_park(plate)
 
+    # return mock plates
     def _scan_plate(self) -> str:
         return "FAKE-" + format(random.randint(0, 999), "03d")
 
 
 class EntrySensor(ParkingSensor):
+    """
+    This sensor will scan cars license plates on carpark entry and register them within the carpark.
+    """
+
     def __init__(self, id: str, is_active: bool, car_park: CarPark) -> None:
         super().__init__(id, is_active, car_park)
 
+    # update carpark implementation to add car.
     def update_car_park(self, data: str | float) -> None:
         if isinstance(data, float):
             raise TypeError("data cannot be float here.")
@@ -68,9 +106,14 @@ class EntrySensor(ParkingSensor):
 
 
 class ExitSensor(ParkingSensor):
+    """
+    This sensor will scan cars license plates on carpark exit and unregister them from the carpark.
+    """
+
     def __init__(self, id: str, is_active: bool, car_park: CarPark) -> None:
         super().__init__(id, is_active, car_park)
 
+    # update carpark implementation to remove car.
     def update_car_park(self, data: str | float) -> None:
         # this feels super sketchy and sorta pointless doing this
         if isinstance(data, float):
